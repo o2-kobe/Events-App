54723f76cac03a9ad7a4cba3029523aa86faa49f
Glenn Rogers <glennrogers186@gmail.com>
2025-06-28 07:58:38 +0000

    feat
    auth

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -1,18 +1,35 @@
+"use client";
 import Link from "next/link";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../(services)/firebaseConfig";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setIsLoggedIn(!!user);
+    });
+  }, []);
+
   return (
     <div className="flex items-center justify-between px-3 py-1 shadow">
       <Link href="/">Events GCTU</Link>
       <div className="flex gap-7 items-center font-semibold">
         <Link href="/events">Events</Link>
         <Link href="/about">About</Link>
-        <Link
-          href="/login"
-          className="py-2 px-4 rounded-full text-sm bg-violet-950 text-white"
-        >
-          Log In
-        </Link>
+
+        {isLoggedIn ? (
+          <Link href="/profile">Profile</Link>
+        ) : (
+          <Link
+            href="/login"
+            className="py-2 px-4 rounded-full text-sm bg-violet-950 text-white"
+          >
+            Log In
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/app/(hooks)/useForm.ts
+++ src/app/(hooks)/useForm.ts
@@ -1,10 +1,18 @@
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../(services)/firebaseConfig";
 
 export default function useForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
@@ -19,8 +27,54 @@ export default function useForm() {
   };
 
   // Define the submit function for log in
+  function handleSubmitLogIn({
+    email,
+    password,
+    event,
+  }: {
+    email: string;
+    password: string;
+    event: React.FormEvent<HTMLFormElement>;
+  }) {
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        router.push("/");
+      })
+      .catch((error) => {
+        alert("Error during sign in:" + error.message);
+      });
+  }
 
   // Define form submit for signUp using the values from useForm
+  function handleSumbitSignUp({
+    username,
+    email,
+    password,
+    event,
+  }: {
+    username: string;
+    email: string;
+    password: string;
+    event: React.FormEvent<HTMLFormElement>;
+  }) {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Save user data to Firestore
+        const userDocRef = doc(db, "users", userCredential.user.uid);
+        return setDoc(userDocRef, {
+          username: username,
+          email: email,
+        });
+      })
+      .then(() => {
+        console.log("User created and data saved to Firestore successfully");
+      })
+      .catch((error) => {
+        alert("Error during sign up:" + error.message);
+      });
+  }
 
   return {
     username,
@@ -31,5 +85,7 @@ export default function useForm() {
     handleUsernameChange,
     showPassword,
     setShowPassword,
+    handleSumbitSignUp,
+    handleSubmitLogIn,
   };
 }
