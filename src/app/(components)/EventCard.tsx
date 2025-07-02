ffea1464057550757be254efdf8a980b40a5b57f
Glenn Rogers <glennrogers186@gmail.com>
2025-07-02 11:04:06 +0000

    commit

--- src/app/(components)/EventCard.tsx
+++ src/app/(components)/EventCard.tsx
@@ -13,7 +13,17 @@ const EventCard: React.FC<EventCardProps> = ({ event, onClick }) => {
   const name = event?.name || "N/A";
   const location = event.location || "N/A";
 
-  const isUpcoming = new Date(event.startDateTime) > new Date();
+  const startDate: Date = (event.startDateTime as any)?.toDate
+    ? (event.startDateTime as any).toDate()
+    : new Date(event.startDateTime);
+  const endDate: Date | undefined = event.endDateTime
+    ? (event.endDateTime as any)?.toDate
+      ? (event.endDateTime as any).toDate()
+      : new Date(event.endDateTime)
+    : undefined;
+
+  const isUpcoming = startDate > new Date();
+
   const formatDate = (date: Date) => {
     return date.toLocaleDateString("en-US", {
       month: "long",
@@ -21,10 +31,8 @@ const EventCard: React.FC<EventCardProps> = ({ event, onClick }) => {
       year: "numeric",
     });
   };
-  const startDateString = formatDate(new Date(event.startDateTime));
-  const endDateString = event.endDateTime
-    ? formatDate(new Date(event.endDateTime))
-    : null;
+  const startDateString = formatDate(startDate);
+  const endDateString = endDate ? formatDate(endDate) : null;
 
   return (
     <div
